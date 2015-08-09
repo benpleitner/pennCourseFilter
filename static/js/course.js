d81ebbe7611002abe2c3173987fa6b342e382b89
Ben <benpleitner@gmail.com>
2015-08-09 15:55:13 -0400

    Added angular components to course page

--- static/js/course.js
+++ static/js/course.js
@@ -1,6 +1,23 @@
 var token = "public";
 var base = "http://api.penncoursereview.com/v1/";
 
+//Angular
+(function() {
+  var app = angular.module("courseRatings", []);
+
+  app.controller("RatingController", function(){
+    this.rating = 2;
+
+    this.setRating = function(value){
+      this.rating = value;
+    };
+
+    this.isSet = function(value){
+      return this.rating === value;
+    };
+  });
+})();
+
 d3.json(base + amplify.store("coursePath") + "/?token=" + token, function(error, rawData) {
 	//Write the course name to the html page
 	var courseName = rawData.result.aliases[0].split("-");
@@ -13,7 +30,7 @@ d3.json(base + amplify.store("coursePath") + "/?token=" + token, function(error,
 
 	//Iterate through each semester
 	for (var i = 0; i < rawData.result.courses.length; i++) {
-		coursePathArray[i] = rawData.result.courses[i].path;
+		// coursePathArray[i] = rawData.result.courses[i].path;
 
 		var year = rawData.result.courses[i].semester.substring(0, rawData.result.courses[i].semester.length - 1),
 				season = rawData.result.courses[i].semester.substring(rawData.result.courses[i].semester.length - 1);
@@ -24,25 +41,29 @@ d3.json(base + amplify.store("coursePath") + "/?token=" + token, function(error,
 	semesterArray.sort();
 
 	for (var i = 0; i < rawData.result.courses.length; i++) {
+		coursePathArray[i] = rawData.result.courses[i].path;
 		semesterNumArray[i] = i;
 		buildData(coursePathArray, semesterArray, semesterNumArray, i);
 	}
 
-	function buildData (coursePathArray, semesterArray, semesterNumArray, i) {
+	function buildData(coursePathArray, semesterArray, semesterNumArray, i) {
 		d3.json(base + coursePathArray[i] + "/reviews/?token=" + token, function(error, data) {
 			var numStudents = data.result.values[0].num_students,
-					semester = semesterArray[i];
+					semester = semesterArray[i],
+					difficulty = data.result.values[0].ratings.rDifficulty;
 
 			forCharts.push({
 				numStudents: numStudents,
 				semester: semester,
-				semesterNum: semesterNumArray[i]
+				semesterNum: semesterNumArray[i],
+				difficulty: difficulty
 			});
 
 			if (i == rawData.result.courses.length - 1) {
 				var ndx = crossfilter(forCharts);
 
 				makeNumStudentsBubbleChart(forCharts, ndx);
+				makeDifficultyBubbleChart(forCharts, ndx);
 			}
 		});
 	}
@@ -68,7 +89,7 @@ d3.json(base + amplify.store("coursePath") + "/?token=" + token, function(error,
 			  .valueAccessor(function (p) {
 			    return p.value;
 			  })
-			  .maxBubbleRelativeSize(0.02)
+			  .maxBubbleRelativeSize(0.015)
 			  .x(d3.scale.linear().domain([-1, semesterArray.length]))
 			  .y(d3.scale.linear().domain([0, 200]))
 				.radiusValueAccessor(function (p) {
@@ -78,20 +99,101 @@ d3.json(base + amplify.store("coursePath") + "/?token=" + token, function(error,
 			  // .elasticY(true)
   			.elasticX(false)
 			  .xAxisLabel("Semester")
-			  .yAxisLabel("Number of Students")
+			  .yAxisLabel("Number of Students (Per Section)")
 			  .renderLabel(false)
-			  // .label(function (p) {
-			  //   return p.key;
-			  // })
+			  .label(function (p) {
+					if (p.key <= semesterArray.length - 1) {
+						if (semesterArray[p.key].split(" ")[1] == "A") {
+							return semesterArray[p.key].split(" ")[0] + " Spring";
+						}
+						else if (semesterArray[p.key].split(" ")[1] == "C"){
+							return semesterArray[p.key].split(" ")[0] + " Fall";
+						}
+						else {
+							return semesterArray[p.key].split(" ")[0] + " Summer";
+						}
+					}
+			  })
 				.renderHorizontalGridLines(true)
 				.transitionDuration(700);
 
 		bubbleChart.xAxis().tickFormat(function (v) {
-			if (semesterArray[v].split(" ")[1] == "A") {
-				return semesterArray[v].split(" ")[0] + " Spring";
+			if (v <= semesterArray.length - 1 && v >= 0) {
+				if (semesterArray[v].split(" ")[1] == "A") {
+					return semesterArray[v].split(" ")[0] + " Spring";
+				}
+				else if (semesterArray[v].split(" ")[1] == "C"){
+					return semesterArray[v].split(" ")[0] + " Fall";
+				}
+				else {
+					return semesterArray[v].split(" ")[0] + " Summer";
+				}
 			}
-			else {
-				return semesterArray[v].split(" ")[0] + " Fall";
+		});
+
+		dc.renderAll();
+	}
+
+	function makeDifficultyBubbleChart(forCharts, ndx) {
+    var semester = ndx.dimension(function(d) {
+      return d.semesterNum;
+    });
+
+    var difficulty = semester.group().reduceSum(function(d) {
+      return d.difficulty;
+    });
+
+		bubbleChart = dc.bubbleChart("#difficultyBubbleChart");
+
+		bubbleChart.width(1200)
+				.height(400)
+				.dimension(semester)
+				.group(difficulty)
+			  .keyAccessor(function (p) {
+			    return p.key;
+			  })
+			  .valueAccessor(function (p) {
+			    return p.value;
+			  })
+			  .maxBubbleRelativeSize(0.015)
+			  .x(d3.scale.linear().domain([-1, semesterArray.length]))
+			  .y(d3.scale.linear().domain([0, 4]))
+				.radiusValueAccessor(function (p) {
+				    return p.value;
+				 })
+  			.r(d3.scale.linear().domain([0, 100]))
+			  // .elasticY(true)
+  			.elasticX(false)
+			  .xAxisLabel("Semester")
+			  .yAxisLabel("Difficulty Rating")
+			  .renderLabel(false)
+			  .label(function (p) {
+					if (p.key <= semesterArray.length - 1) {
+						if (semesterArray[p.key].split(" ")[1] == "A") {
+							return semesterArray[p.key].split(" ")[0] + " Spring";
+						}
+						else if (semesterArray[p.key].split(" ")[1] == "C"){
+							return semesterArray[p.key].split(" ")[0] + " Fall";
+						}
+						else {
+							return semesterArray[p.key].split(" ")[0] + " Summer";
+						}
+					}
+			  })
+				.renderHorizontalGridLines(true)
+				.transitionDuration(700);
+
+		bubbleChart.xAxis().tickFormat(function (v) {
+			if (v <= semesterArray.length - 1 && v >= 0) {
+				if (semesterArray[v].split(" ")[1] == "A") {
+					return semesterArray[v].split(" ")[0] + " Spring";
+				}
+				else if (semesterArray[v].split(" ")[1] == "C"){
+					return semesterArray[v].split(" ")[0] + " Fall";
+				}
+				else {
+					return semesterArray[v].split(" ")[0] + " Summer";
+				}
 			}
 		});
 
